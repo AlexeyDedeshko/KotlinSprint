6a13b9ae712f21dfa336db7da195058e445e8d7b
alexeydedeshko <905advo@gmai.com>
2024-02-25 00:54:25 +0300

    Initial Commit test 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World!111111")
+    println("Hello World!2222222")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
